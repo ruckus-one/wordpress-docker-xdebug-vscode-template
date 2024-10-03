a08a8bab3973854c266f0082c9edb51b06ae693c
Bart Karalus <bart@bforge.co>
2024-10-03 19:34:25 +0200

    feat: wp-cli added during environment setup

--- config/Dockerfile.php
+++ config/Dockerfile.php
@@ -1,6 +1,10 @@
 FROM wordpress:php8.3-fpm
 
-RUN apt-get update && apt-get install -y iputils-ping
+RUN apt-get update && apt-get install -y iputils-ping less mariadb-server
 
 RUN pecl install xdebug && docker-php-ext-enable xdebug
 COPY xdebug.ini /usr/local/etc/php/conf.d/docker-php-ext-xdebug.ini
+
+RUN curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar && \
+    chmod +x wp-cli.phar && \
+    mv wp-cli.phar /usr/local/bin/wp
